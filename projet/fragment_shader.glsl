87d75aae70c085742739166a8280f5b18752c3b2
ossm29 <77997318+ossm29@users.noreply.github.com>
2021-04-19 23:44:34 +0200

    Update fragment_shader.glsl

--- projet/fragment_shader.glsl
+++ projet/fragment_shader.glsl
@@ -1,19 +1,19 @@
 #ifdef GL_ES
-precision mediump float;
+  precision mediump float;
 precision mediump int;
 #endif
 
 uniform sampler2D texture;
 
-smooth in vec4 vertColor
-smooth in vec4 verTexCoord;
+smooth in vec4 vertColor;
+smooth in vec4 vertTexCoord;
 
 smooth in vec2 vertHeat;
 
 void main() {
   gl_FragColor = texture2D(texture, vertTexCoord.st) * vertColor;
-  gl_FragColor.r = 255/(25*verthHeat[0])
-  gl_FragColor.v = 255/(25*verthHeat[1])
-  gl_FragColor.b = 255/(25*verthHeat[2])
-
+  if (vertHeat[0] != 0 && vertHeat[1] != 0) {
+    gl_FragColor.r += 255.0/(5*vertHeat[0]+1);
+    gl_FragColor.g += 255.0/(5*vertHeat[1]+1);
+  }
 }
